d44d6e9b714344234d2ce1339ac37101862f3a30
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-09-09 18:34:44 +0100

    feat: created AppLayout component for consistent page structure

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+
+function AppLayout() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-between">
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default AppLayout;
