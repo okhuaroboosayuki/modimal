6112423519e18f9b25f56ce15e73bb272c69a9a4
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-09-19 22:39:16 +0100

    feat: extended Input component to accept value and onChange props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,20 @@
-function Input({ type, name, placeholder, width, customStyle }) {
+function Input({
+  type,
+  name,
+  placeholder,
+  width,
+  customStyle,
+  value,
+  onChange,
+}) {
   return (
     <input
       type={type}
       name={name}
       id={name}
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
       className={`${width} ${customStyle} border-none text-[16px] outline-none placeholder:text-xs`}
       spellCheck={type === "search" && "true"}
       aria-label={type === "search" && "Search through site content"}
