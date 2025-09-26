5653eb3d9048aaacf83c3855d8c7c93c6f71f8a1
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-09-26 21:27:54 +0100

    feat: improved useSearchProducts hook to include sorting and filtering parameters

--- src/features/products/useSearchProducts.js
+++ src/features/products/useSearchProducts.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
-import { searchProducts } from "../../services/apiProducts";
+import { getProducts } from "../../services/apiProducts";
 
 export function useSearchProducts() {
   const [searchParams] = useSearchParams();
   const searchQuery = searchParams.get("q");
 
+  // Get all other filter parameters.
+  const filters = {
+    fabric: searchParams.getAll("fabric"),
+    color: searchParams.getAll("color"),
+    size: searchParams.getAll("size"),
+    collection: searchParams.getAll("collection"),
+  };
+
+  console.log(filters);
+
+  // // Get the sort parameter
+  const sortBy = searchParams.get("sort");
+
   const { data, isPending: isSearching } = useQuery({
-    queryKey: ["searchedProducts", searchQuery],
-    queryFn: () => searchProducts(searchQuery),
+    queryKey: ["searchedProducts", searchQuery, sortBy, filters],
+    queryFn: () => getProducts(searchQuery, sortBy, filters),
   });
 
   return { data, isSearching };
