ef95060bb5afc06457f18c657ec2d8b151836c8c
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-09-30 22:11:59 +0100

    feat: refactored SearchPage to improve product listing and search handling

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,28 +1,47 @@
 import { useSearchProducts } from "../features/products/useSearchProducts";
-import ProductsListContainer from "../components/products/ProductsListContainer";
+import ProductsList from "../components/products/ProductsList";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { setProducts } from "../features/products/filterSlice";
 
 function SearchPage() {
   const { data, isSearching } = useSearchProducts();
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get("q");
+  const previousSearchQuery = useRef(null);
+
+  useEffect(() => {
+    if (data && searchQuery !== previousSearchQuery.current) {
+      dispatch(setProducts(data));
+      previousSearchQuery.current = searchQuery;
+    }
+  }, [data, searchQuery, dispatch]);
 
   const products = data?.data || [];
   const totalItems = products.length || 0;
 
-  if (isSearching) return <div>Searching...</div>;
-
-  if (!products || products.length === 0) {
-    return <p>No products available.</p>;
-  }
-
   return (
-    <section className="flex w-full flex-col items-center justify-center gap-10 px-[108px] py-12">
-      <div>search input</div>
+    <>
+      <div className="grid-head flex w-full flex-col items-center gap-10">
+        <div>search input</div>
 
-      <div className="flex w-full flex-col items-center justify-center gap-8 capitalize">
         <p className="text-[20px]">{totalItems} items</p>
-
-        <ProductsListContainer products={products} />
       </div>
-    </section>
+
+      {isSearching ? (
+        <div className="flex w-full items-center justify-center">
+          Searching...
+        </div>
+      ) : !products || products.length === 0 ? (
+        <p className="flex w-full items-center justify-center">
+          No products available.
+        </p>
+      ) : (
+        <ProductsList products={products} />
+      )}
+    </>
   );
 }
 
