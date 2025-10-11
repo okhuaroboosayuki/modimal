0620ceb1cc3d9e09a53debd8d5d9e682533da8d9
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-11 20:55:37 +0100

    feat: implemented lazy loading for routes and added NotFound page; removed ProductDetailsLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+
 import AppLayout from "./components/AppLayout";
-import Home from "./pages/Home";
-import SearchPage from "./pages/SearchPage";
-import ProductDetailsLayout from "./components/ProductDetailsLayout";
+import NotFound from "./pages/NotFound";
+
+const Home = lazy(() => import("./pages/Home"));
+const SearchPage = lazy(() => import("./pages/SearchPage"));
+const AllProducts = lazy(() => import("./pages/AllProducts"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,15 +25,16 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
 
       <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Home />} />
-
-            <Route element={<ProductDetailsLayout />}>
+        <Suspense fallback={<div>loading</div>}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<Home />} />
               <Route path="search" element={<SearchPage />} />
+              <Route path="shop-all" element={<AllProducts />} />
+              <Route path="*" element={<NotFound />} />
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
 
       <Toaster
