6436c3a32b2c9ac7e0856a5dea700ae33abb63d6
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-12 12:21:43 +0100

    fix: added padding to filter container when modal is open for better spacing

--- src/components/filter/FilterContainer.jsx
+++ src/components/filter/FilterContainer.jsx
@@ -5,7 +5,7 @@ import Filters from "./Filters";
 function FilterContainer({ closeModal }) {
   return (
     <aside
-      className={`filter-container mt-7 ${closeModal ? "flex" : "hidden"} flex-col gap-4 ${!closeModal && "lg:flex"} lg:w-[300px] xl:w-[408px]`}
+      className={`filter-container mt-7 ${closeModal ? "flex px-5" : "hidden"} flex-col gap-4 ${!closeModal && "lg:flex"} lg:w-[300px] xl:w-[408px]`}
     >
       <div className="flex items-center justify-between">
         <h3 className="text-[2rem] font-semibold">Filters</h3>
