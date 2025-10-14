23a104534d4c01e2fa2d32f4b1119d49f3cd8e8a
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-14 23:23:08 +0100

    feat: created a reusable products component for display results in various product pages

---
+++ src/features/products/ProductsPageContent.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setProducts } from "../filter/filterSlice";
+import toast from "react-hot-toast";
+import FilterContainer from "../../components/filter/FilterContainer";
+import Modal from "./../modal/Modal";
+import ProductsList from "./../../components/products/ProductsList";
+import EmptyProduct from "../../components/products/EmptyProduct";
+import { LoadingSpinner } from "../../components/Loaders";
+import MobileFilter from "../../components/filter/MobileFilter";
+import MobileFilterButton from "../../components/filter/MobileFilterButton";
+
+function ProductsPageContent({ data, loader }) {
+  const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
+  const hasDispatched = useRef(false);
+  const searchQuery = searchParams.get("q");
+  const previousSearchQuery = useRef(null);
+
+  useEffect(() => {
+    if (searchQuery) {
+      if (data && searchQuery !== previousSearchQuery.current) {
+        dispatch(setProducts(data));
+        previousSearchQuery.current = searchQuery;
+        return;
+      }
+    }
+
+    if (data && !hasDispatched.current) {
+      dispatch(setProducts(data));
+      hasDispatched.current = true;
+    }
+  }, [data, dispatch, searchQuery]);
+
+  const products = data?.data || [];
+
+  if (data && products.length === 0) {
+    toast.error(`Products could not be loaded`);
+  }
+
+  return (
+    <Modal>
+      <section className="product-container">
+        <FilterContainer />
+
+        <section className="grid-head flex w-full flex-col items-center justify-center gap-10">
+          <>
+            <Modal.Open opens={"mobile-filter"}>
+              <MobileFilterButton />
+            </Modal.Open>
+            <Modal.Window
+              name={"mobile-filter"}
+              containerId={"root"}
+              styles={"filter-modal"}
+            >
+              <MobileFilter />
+            </Modal.Window>
+          </>
+        </section>
+
+        {loader ? (
+          <div className="grid-body mt-9 w-full">
+            <LoadingSpinner />
+          </div>
+        ) : !products || products.length === 0 ? (
+          <div className="grid-body mt-9 w-full">
+            <EmptyProduct />
+          </div>
+        ) : (
+          <ProductsList products={products} />
+        )}
+      </section>
+    </Modal>
+  );
+}
+
+export default ProductsPageContent;
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,76 +1,10 @@
-import { useEffect, useRef } from "react";
-import { useSearchParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import toast from "react-hot-toast";
+import ProductsPageContent from "../features/products/ProductsPageContent";
 import { useSearchProducts } from "../features/products/useSearchProducts";
-import ProductsList from "../components/products/ProductsList";
-import { setProducts } from "../features/filter/filterSlice";
-import MobileFilter from "../components/filter/MobileFilter";
-import Modal from "../features/modal/Modal";
-import MobileFilterButton from "../components/filter/MobileFilterButton";
-import { LoadingSpinner } from "../components/Loaders";
-import EmptyProduct from "../components/products/EmptyProduct";
-import FilterContainer from "../components/filter/FilterContainer";
 
 function SearchPage() {
   const { data, isSearching } = useSearchProducts();
-  const dispatch = useDispatch();
-  const [searchParams] = useSearchParams();
-  const searchQuery = searchParams.get("q");
-  const previousSearchQuery = useRef(null);
 
-  useEffect(() => {
-    if (data && searchQuery !== previousSearchQuery.current) {
-      dispatch(setProducts(data));
-      previousSearchQuery.current = searchQuery;
-    }
-  }, [data, searchQuery, dispatch]);
-
-  const products = data?.data || [];
-  const totalItems = products.length || 0;
-
-  if (data && products.length === 0) {
-    toast.error(`Products with "${searchQuery}" are unavailable`);
-  }
-
-  return (
-    <Modal>
-      <section className="product-container">
-        <FilterContainer />
-
-        <section className="grid-head flex w-full flex-col items-center gap-10">
-          <div>search input</div>
-
-          <p className="hidden text-[20px] lg:block">{totalItems} items</p>
-
-          <>
-            <Modal.Open opens={"mobile-filter"}>
-              <MobileFilterButton />
-            </Modal.Open>
-            <Modal.Window
-              name={"mobile-filter"}
-              containerId={"root"}
-              styles={"filter-modal"}
-            >
-              <MobileFilter />
-            </Modal.Window>
-          </>
-        </section>
-
-        {isSearching ? (
-          <div className="grid-body mt-9 w-full">
-            <LoadingSpinner />
-          </div>
-        ) : !products || products.length === 0 ? (
-          <div className="grid-body mt-9 w-full">
-            <EmptyProduct />
-          </div>
-        ) : (
-          <ProductsList products={products} />
-        )}
-      </section>
-    </Modal>
-  );
+  return <ProductsPageContent data={data} loader={isSearching} />;
 }
 
 export default SearchPage;
