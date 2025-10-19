38daaa2a53bcc9209f783f1588e1748982c2a3d6
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-19 23:50:18 +0100

    fix: added hero image to plus-size page

--- src/pages/PlusSize.jsx
+++ src/pages/PlusSize.jsx
@@ -1,10 +1,17 @@
 import ProductsPageContent from "../features/products/ProductsPageContent";
 import { usePlusSizeProducts } from "../features/products/usePlusSizeProducts";
+import PlusSizeHero from "/images/plus_size_hero.png";
 
 function PlusSize() {
   const { isPlusSizeProductLoading, data } = usePlusSizeProducts();
 
-  return <ProductsPageContent data={data} loader={isPlusSizeProductLoading} />;
+  return (
+    <ProductsPageContent
+      data={data}
+      loader={isPlusSizeProductLoading}
+      heroImage={PlusSizeHero}
+    />
+  );
 }
 
 export default PlusSize;
