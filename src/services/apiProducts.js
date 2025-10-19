92fcf8716150f9676a2997ba68aff61d49ed698f
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-19 23:53:37 +0100

    feat: added function for bestselling products; tweaked modiweek, new-products, and search functions

--- src/services/apiProducts.js
+++ src/services/apiProducts.js
@@ -9,13 +9,17 @@ export async function getProducts({
   sortBy = null,
   filters = {},
 }) {
-  let query = supabase.from("products").select("*");
+  let query;
 
   //if searching
   if (searchValue !== null) {
-    query = query.or(
-      `productName.ilike.%${searchValue}%, category.ilike.%${searchValue}%, productTag.ilike.%${searchValue}%, fabricDetails.cs.{"type": "${searchValue}"}, fabricDetails.cs.{"tags": ["${searchValue}"]}, modiweek.ilike.%${searchValue}%, availableColors.cs.{"${searchValue}"}`,
-    );
+    query = supabase.rpc("search_products", { search: `${searchValue}` });
+  } else {
+    query = supabase
+      .from("products")
+      .select(
+        "id, created_at, productName, category, price, availableColors, fabricDetails, stockQuantity, totalSold, availableSizes, productTag, productImages, discountPercentage, modiweek",
+      );
   }
 
   // Apply sorting and filters
@@ -37,7 +41,11 @@ export async function getProductsByCategory(
   category,
   { sortBy = null, filters = {} },
 ) {
-  let query = supabase.from("products").select("*");
+  let query = supabase
+    .from("products")
+    .select(
+      "id, created_at, productName, category, price, availableColors, fabricDetails, stockQuantity, totalSold, availableSizes, productTag, productImages, discountPercentage, modiweek",
+    );
 
   if (category) {
     query = query.eq("category", category);
@@ -58,15 +66,77 @@ export async function getProductsByCategory(
 
 // get new products
 export async function getNewProducts({ sortBy = null, filters = {} }) {
-  let query = supabase.from("products").select("*");
+  let query = supabase
+    .from("products")
+    .select(
+      "id, created_at, productName, category, price, availableColors, fabricDetails, stockQuantity, totalSold, availableSizes, productTag, productImages, discountPercentage, modiweek",
+    );
 
-  const sevenDaysAgo = subDays(new Date(), 7).toISOString();
+  const fourteenDaysAgo = subDays(new Date(), 14).toISOString();
 
-  query = query.gte("created_at", sevenDaysAgo);
+  query = query.gte("created_at", fourteenDaysAgo);
   query = applySort(query, sortBy);
   query = applyFilters(query, filters);
 
   const { data, error } = await query.order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    toast.error("Products could not be loaded");
+  }
+
+  return { data };
+}
+
+export async function getProductsByModiweek({ sortBy, filters }) {
+  let query = supabase
+    .from("products")
+    .select(
+      "id, created_at, productName, category, price, availableColors, fabricDetails, stockQuantity, totalSold, availableSizes, productTag, productImages, discountPercentage, modiweek",
+    );
+
+  query = query.neq("modiweek", null);
+  query = applySort(query, sortBy);
+  query = applyFilters(query, filters);
+
+  const { data, error } = await query.order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    toast.error("Products could not be loaded");
+  }
+
+  return { data };
+}
+
+export async function getProductsByPlusSize({ sortBy, filters }) {
+  let query = supabase.rpc("get_products_with_plus_size", { _size: "XL" });
+
+  query = applySort(query, sortBy);
+  query = applyFilters(query, filters);
+
+  const { data, error } = await query.order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    toast.error("Products could not be loaded");
+  }
+
+  return { data };
+}
+
+export async function getProductsByBestSeller({ sortBy, filters }) {
+  let query = supabase
+    .from("products")
+    .select(
+      "id, created_at, productName, category, price, availableColors, fabricDetails, stockQuantity, totalSold, availableSizes, productTag, productImages, discountPercentage, modiweek",
+    );
+
+  query = query.order("totalSold", { ascending: false });
+  query = applySort(query, sortBy);
+  query = applyFilters(query, filters);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
