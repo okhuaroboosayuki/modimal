ef358126c67b6f479154cd9a9ee895246524c805
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-19 22:36:27 +0100

    feat: added a Link component for url navigation for Navigation option that is not a modal

--- src/components/header/nav/NavTitle.jsx
+++ src/components/header/nav/NavTitle.jsx
@@ -1,4 +1,17 @@
+import { Link } from "react-router-dom";
+
 function NavTitle({ title, onClick }) {
+  if (!onClick) {
+    return (
+      <Link
+        to={`/${title}`}
+        className="transition-500-in-out hover:text-neutral-black capitalize"
+      >
+        {title}
+      </Link>
+    );
+  }
+
   return (
     <li
       className="transition-500-in-out hover:text-neutral-black cursor-pointer capitalize"
