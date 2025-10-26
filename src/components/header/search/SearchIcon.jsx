d8908be4e5649eab9fb6b3b5f3fa99fa53441c9d
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-10-26 14:06:15 +0100

    feat: updated SearchIcon to handle search click based on search query state

--- src/components/header/search/SearchIcon.jsx
+++ src/components/header/search/SearchIcon.jsx
@@ -1,13 +1,29 @@
 import { IoMdSearch } from "react-icons/io";
 import { RiCloseFill } from "react-icons/ri";
+import { useSelector } from "react-redux";
 
 function SearchIcon({ windowName, onClick, customStyle }) {
+  const { searchQueryState } = useSelector((store) => store.searchReducer);
+
   const isWindowNameSearch =
     windowName === "search" || windowName === "mobile-search";
 
+  const handleSearchClick = () => {
+    if (!searchQueryState) {
+      onClick();
+    }
+  };
+
+  const searchStateCheck = searchQueryState
+    ? searchQueryState
+    : isWindowNameSearch;
+
   return (
-    <li className={`${customStyle} icon cursor-pointer`} onClick={onClick}>
-      {isWindowNameSearch ? <RiCloseFill /> : <IoMdSearch />}
+    <li
+      className={`${customStyle} icon cursor-pointer`}
+      onClick={handleSearchClick}
+    >
+      {searchStateCheck ? <RiCloseFill /> : <IoMdSearch />}
     </li>
   );
 }
