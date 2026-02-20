99ac82fa097bd286b76ab3a328a0d70ff1cad258
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-02-20 14:54:34 +0100

    refactor: simplified conditional rendering in NavImageCard component

--- src/components/header/nav/NavImageCard.jsx
+++ src/components/header/nav/NavImageCard.jsx
@@ -2,15 +2,21 @@ import { Link } from "react-router-dom";
 
 function NavImageCard({ src, alt, title, url, onCloseModal }) {
   return (
-    <Link
-      to={url}
-      onClick={onCloseModal}
-      className="flex flex-col items-start gap-3 capitalize"
-    >
-      <img src={src} alt={alt} loading="lazy" height={420} />
+    <>
+      {!url && !title ? (
+        <img src={src} alt={alt} loading="lazy" height={420} />
+      ) : (
+        <Link
+          to={url}
+          onClick={onCloseModal}
+          className="flex flex-col items-start gap-3 capitalize"
+        >
+          <img src={src} alt={alt} loading="lazy" height={420} />
 
-      <p>{title}</p>
-    </Link>
+          <p>{title}</p>
+        </Link>
+      )}
+    </>
   );
 }
 
