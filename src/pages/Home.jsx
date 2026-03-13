3df173634c5c9525482efe8f5ea9337a788c7809
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-03-13 11:56:23 +0100

    feat: added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// import Uploader from "../data/Uploader";
+
+function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      {/* <div aria-disabled> */}
+      {/* <Uploader /> */}
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default Home;
