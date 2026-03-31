9506927ff0341eaff9501ba8b62326721d1f9f4c
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-04-01 00:57:28 +0100

    refactor: moved filter options to constants file for better organization and reusability

--- src/components/filter/Filters.jsx
+++ src/components/filter/Filters.jsx
@@ -2,31 +2,15 @@ import { useSelector } from "react-redux";
 import FilterField from "./FilterField";
 import SortField from "./SortField";
 import { selectColors, selectFabrics } from "../../features/filter/filterSlice";
+import {
+  collectionOptions,
+  sizeOptions,
+  sortOptions,
+} from "../../utils/contants";
 
 function Filters() {
   const colors = useSelector(selectColors);
   const fabrics = useSelector(selectFabrics);
-
-  const sortOptions = [
-    { value: "featured", label: "featured" },
-    { value: "bestseller", label: "best seller" },
-    { value: "price-asc", label: "price: low to high" },
-    { value: "price-desc", label: "price: high to low" },
-  ];
-
-  const sizeOptions = [
-    { value: "XS", label: "xs / us (0-4)" },
-    { value: "S", label: "s / us (4-6)" },
-    { value: "M", label: "m / us (6-10)" },
-    { value: "L", label: "l / us (10-14)" },
-    { value: "XL", label: "xl / us (14-18)" },
-    { value: "XXL", label: "xxl / us (18-22)" },
-  ];
-
-  const collectionOptions = [
-    { value: "inStock", label: "in stock" },
-    { value: "outOfStock", label: "out of stock" },
-  ];
 
   return (
     <section className="flex w-full flex-col gap-4">
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,1 +1,22 @@
 export const PAGE_SIZE = 6;
+
+export const sizeOptions = [
+  { value: "XS", label: "xs / us (0-4)" },
+  { value: "S", label: "s / us (4-6)" },
+  { value: "M", label: "m / us (6-10)" },
+  { value: "L", label: "l / us (10-14)" },
+  { value: "XL", label: "xl / us (14-18)" },
+  { value: "XXL", label: "xxl / us (18-22)" },
+];
+
+export const sortOptions = [
+  { value: "featured", label: "featured" },
+  { value: "bestseller", label: "best seller" },
+  { value: "price-asc", label: "price: low to high" },
+  { value: "price-desc", label: "price: high to low" },
+];
+
+export const collectionOptions = [
+  { value: "inStock", label: "in stock" },
+  { value: "outOfStock", label: "out of stock" },
+];
