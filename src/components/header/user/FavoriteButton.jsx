edf1754efde48d9e066612ae85c701973e5667f3
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-04-07 12:22:03 +0100

    refactor: made changes to FavoriteButton component to include navigation on click

--- src/components/header/user/FavoriteButton.jsx
+++ src/components/header/user/FavoriteButton.jsx
@@ -1,14 +1,18 @@
 import { RiHeartFill } from "react-icons/ri";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import HeartIcon from "../../icons/HeartIcon";
 
 function FavoriteButton() {
+  const navigate = useNavigate();
   const location = useLocation();
 
   return (
     <li className="icon">
       {location.pathname !== "/favorites" ? (
-        <HeartIcon className="cursor-pointer" />
+        <HeartIcon
+          className="cursor-pointer"
+          onClick={() => navigate("/favorites")}
+        />
       ) : (
         <RiHeartFill fill="red" />
       )}
