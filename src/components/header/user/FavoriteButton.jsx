40e2b2b7684199d9a46885ade4c9b7e2c3f1e23d
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-04-07 12:14:29 +0100

    refactor: simplified FavoriteButton component by removing unused navigate function

--- src/components/header/user/FavoriteButton.jsx
+++ src/components/header/user/FavoriteButton.jsx
@@ -1,18 +1,14 @@
 import { RiHeartFill } from "react-icons/ri";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import HeartIcon from "../../icons/HeartIcon";
 
 function FavoriteButton() {
-  const navigate = useNavigate();
   const location = useLocation();
 
   return (
     <li className="icon">
       {location.pathname !== "/favorites" ? (
-        <HeartIcon
-          className="cursor-pointer"
-          onClick={() => navigate("/favorites")}
-        />
+        <HeartIcon className="cursor-pointer" />
       ) : (
         <RiHeartFill fill="red" />
       )}
