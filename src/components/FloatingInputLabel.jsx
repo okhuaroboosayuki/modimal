c53ca0b32278915af6709fa7e10887f8e1633b51
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-04-23 13:21:19 +0100

    feat: added className prop to FloatingInputLabel for customizable styling

--- src/components/FloatingInputLabel.jsx
+++ src/components/FloatingInputLabel.jsx
@@ -1,8 +1,14 @@
-function FloatingInputLabel({ name, isFloating, placeholder, error }) {
+function FloatingInputLabel({
+  name,
+  isFloating,
+  placeholder,
+  error,
+  className,
+}) {
   return (
     <label
       htmlFor={name}
-      className={`transition-500-in-out pointer-events-none absolute left-4 px-1 text-xs ${isFloating ? "text-primary-300 top-0 -translate-y-1/2 bg-white text-xs font-semibold" : `text-gray60 ${error ? "top-1/3" : "top-1/2"} -translate-y-1/2 text-sm`} `}
+      className={`transition-500-in-out pointer-events-none absolute left-4 px-1 text-xs ${isFloating ? "text-primary-300 top-0 -translate-y-1/2 bg-white text-xs font-semibold" : `text-gray60 ${error ? "top-[50%]" : "top-1/2"} -translate-y-1/2 text-sm`} ${className}`}
     >
       {placeholder}
     </label>
