a994bf8120e5bc03c31e734e8353d214ff208944
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-04-29 22:29:37 +0100

    refactor: simplified CartInfo component by removing unused form logic and rendering InfoPageDetails

---
+++ src/features/cart/InfoPageDetails.jsx
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../auth/useUser";
+import CheckoutFormActions from "../../components/cart/CheckoutFormActions";
+import CheckBox from "../../components/CheckBox";
+import { useCheckoutForm } from "../../hooks/useCheckoutForm";
+import CartContactDetails from "./CartContactDetails";
+import CartShippingDetails from "./CartShippingDetails";
+
+function InfoPageDetails() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useUser();
+  const { register, handleSubmit } = useCheckoutForm();
+
+  const onSubmit = ({
+    email,
+    saveEmail,
+    country,
+    firstName,
+    lastName,
+    company,
+    address,
+    apartment,
+    postalCode,
+    state,
+    saveShippingAddress,
+  }) => {
+    if (!email || !country || !firstName || !lastName || !address || !state)
+      return;
+    console.log(email);
+    console.log(saveEmail);
+    console.log(country.toLowerCase());
+    console.log(firstName);
+    console.log(lastName);
+    console.log(company);
+    console.log(address);
+    console.log(apartment);
+    console.log(postalCode);
+    console.log(state.toLowerCase());
+    console.log(saveShippingAddress);
+  };
+
+  return (
+    <form
+      className="flex w-full flex-col items-start gap-12"
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") e.preventDefault();
+      }}
+    >
+      <div className="flex w-full flex-col items-start gap-6">
+        <div className="flex w-full flex-col items-start gap-2">
+          <CartContactDetails />
+
+          <CheckBox
+            label={"email me with news and offers"}
+            {...register("saveEmail")}
+          />
+        </div>
+
+        <div className="flex w-full flex-col items-start gap-2">
+          <CartShippingDetails />
+
+          <CheckBox
+            label={"save this information for next time"}
+            {...register("saveShippingAddress")}
+          />
+        </div>
+      </div>
+
+      <CheckoutFormActions
+        btnText={
+          !isAuthenticated ? "log in to continue" : "continue to shipping"
+        }
+        linkText={"return to cart"}
+        url={"/cart"}
+        onClick={
+          !isAuthenticated
+            ? (e) => {
+                e.preventDefault();
+                navigate("/login?pp=cartInfo");
+              }
+            : ""
+        }
+      />
+    </form>
+  );
+}
+
+export default InfoPageDetails;
--- src/pages/CartInfo.jsx
+++ src/pages/CartInfo.jsx
@@ -1,42 +1,7 @@
-// import { useForm } from "react-hook-form";
-import CartContactDetails from "../features/cart/CartContactDetails";
-import CheckBox from "../components/CheckBox";
-import Button from "../components/Button";
-import { useCheckoutForm } from "../hooks/useCheckoutForm";
-import CartShippingDetails from "../features/cart/CartShippingDetails";
+import InfoPageDetails from "../features/cart/InfoPageDetails";
 
 function CartInfo() {
-  const { register, handleSubmit } = useCheckoutForm();
-
-  const onSubmit = ({ email, emailCheckBox, country }) => {
-    if (!email) return;
-    console.log(email);
-    console.log(emailCheckBox);
-    console.log(country);
-  };
-
-  return (
-    <form
-      className="flex w-full flex-col items-start gap-6"
-      onSubmit={handleSubmit(onSubmit)}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") e.preventDefault();
-      }}
-    >
-      <div className="flex w-full flex-col items-start gap-2">
-        <CartContactDetails />
-
-        <CheckBox
-          label={"email me with news and offers"}
-          {...register("emailCheckBox")}
-        />
-      </div>
-
-      <CartShippingDetails />
-
-      <Button>continue to shipping</Button>
-    </form>
-  );
+  return <InfoPageDetails />;
 }
 
 export default CartInfo;
