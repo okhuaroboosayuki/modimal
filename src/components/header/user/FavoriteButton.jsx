6bc9cb864ac2454e07eb1dd742cbd99b2c5a5d03
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-05-15 20:23:30 +0100

    fix: updated FavoriteButton to conditionally navigate based on authentication status

--- src/components/header/user/FavoriteButton.jsx
+++ src/components/header/user/FavoriteButton.jsx
@@ -1,17 +1,19 @@
 import { RiHeartFill } from "react-icons/ri";
 import { useLocation, useNavigate } from "react-router-dom";
 import HeartIcon from "../../icons/HeartIcon";
+import { useUser } from "../../../features/auth/useUser";
 
 function FavoriteButton() {
   const navigate = useNavigate();
   const location = useLocation();
+  const { isAuthenticated } = useUser();
 
   return (
     <li className="icon">
       {location.pathname !== "/favorites" ? (
         <HeartIcon
           className="cursor-pointer"
-          onClick={() => navigate("/favorites")}
+          onClick={isAuthenticated ? () => navigate("/favorites") : undefined}
         />
       ) : (
         <RiHeartFill fill="red" />
