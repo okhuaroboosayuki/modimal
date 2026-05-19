fd65ff077c37ce911b690f378e9c822142ea4a16
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-05-19 17:05:36 +0100

    feat: added shipping details dispatch in CheckoutFormProvider based on user metadata

--- src/context/CheckoutFormProvider.jsx
+++ src/context/CheckoutFormProvider.jsx
@@ -1,11 +1,46 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useUser } from "../features/auth/useUser";
 import { CheckoutFormContext } from "./CheckoutFormContext";
+import { setShippingDetails } from "../features/cart/checkoutSlice";
 
 export function CheckoutFormProvider({ children }) {
+  const dispatch = useDispatch();
   const { data } = useUser();
   const { shippingDetails } = useSelector((store) => store.checkoutReducer);
+
+  useEffect(() => {
+    if (shippingDetails || !data || data?.aud !== "authenticated") return;
+
+    const metadata = data.user_metadata || {};
+    const hasUserShippingData =
+      metadata.fullName ||
+      metadata.address ||
+      metadata.country ||
+      metadata.state ||
+      metadata.postalCode ||
+      metadata.phone ||
+      metadata.company ||
+      metadata.apartment;
+
+    if (!hasUserShippingData) return;
+
+    dispatch(
+      setShippingDetails({
+        email: data.email ?? "",
+        subscribeToNewsletter: metadata.subscribeToNewsletter ?? false,
+        country: metadata.country ?? "",
+        company: metadata.company ?? "",
+        apartment: metadata.apartment ?? "",
+        postalCode: metadata.postalCode ?? "",
+        fullName: metadata.fullName ?? "",
+        state: metadata.state ?? "",
+        address: metadata.address ?? "",
+        phone: metadata.phone ?? "",
+      }),
+    );
+  }, [data, dispatch, shippingDetails]);
 
   const methods = useForm({
     values: {
@@ -21,11 +56,11 @@ export function CheckoutFormProvider({ children }) {
       postalCode:
         (data?.user_metadata?.postalCode || shippingDetails?.postalCode) ?? "",
       firstName:
-        data?.user_metadata?.fullName.split(" ")[0] ||
-        shippingDetails?.fullName.split(" ")[0],
+        data?.user_metadata?.fullName?.split(" ")[0] ||
+        shippingDetails?.fullName?.split(" ")[0],
       lastName:
-        data?.user_metadata?.fullName.split(" ")[1] ||
-        shippingDetails?.fullName.split(" ")[1],
+        data?.user_metadata?.fullName?.split(" ")[1] ||
+        shippingDetails?.fullName?.split(" ")[1],
       state: (data?.user_metadata?.state || shippingDetails?.state) ?? "",
       address: (data?.user_metadata?.address || shippingDetails?.address) ?? "",
       phone: (data?.user_metadata?.phone || shippingDetails?.phone) ?? "",
