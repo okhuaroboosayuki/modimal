80664abae6b3e81eac5d01f0996aa01fde7235a9
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-05-21 11:59:50 +0100

    feat: passed and accessed totalAmount to CheckoutLeftPanel and Outlet; adding to local state used when making payments

--- src/components/cart/CartFlowLayout.jsx
+++ src/components/cart/CartFlowLayout.jsx
@@ -37,7 +37,10 @@ function CartFlowLayout() {
       className="relative flex w-full flex-col-reverse gap-2 text-sm sm:text-lg lg:grid lg:h-screen lg:grid-cols-2 lg:grid-rows-1"
       id="checkoutFlow"
     >
-      <CheckoutLeftPanel totalCartCount={totalCartCount} />
+      <CheckoutLeftPanel
+        totalCartCount={totalCartCount}
+        totalAmount={totalAmount}
+      />
 
       <CheckoutRightPanel
         cartItems={derivedCart}
--- src/components/cart/CheckoutLeftPanel .jsx
+++ src/components/cart/CheckoutLeftPanel .jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import CartFlowBreadCrumbs from "./CartFlowBreadCrumbs";
 import CartPageHeader from "./CartPageHeader";
 
-function CheckoutLeftPanel({ totalCartCount }) {
+function CheckoutLeftPanel({ totalCartCount, totalAmount }) {
   return (
     <div className="order-2 flex w-full flex-col items-start justify-start gap-8 px-5 sm:px-8 lg:order-1 lg:px-12 lg:pt-8 xl:px-15">
       <CartPageHeader />
@@ -12,7 +12,7 @@ function CheckoutLeftPanel({ totalCartCount }) {
           <CartFlowBreadCrumbs />
 
           <section className="hide-scrollbar absolute left-0 flex w-full flex-col items-start justify-between px-5 py-5 text-sm capitalize max-lg:top-[100%] sm:px-8 sm:text-lg lg:relative lg:overflow-y-scroll lg:px-0">
-            <Outlet />
+            <Outlet context={{ totalAmount }} />
           </section>
         </>
       )}
