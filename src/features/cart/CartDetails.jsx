8e69ae4e81a56068294aa9fba539c3fd9aef40ed
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-05-24 20:55:09 +0100

    feat: updated CartDetails to calculate shipping fee and total amount based on guaranteed delivery

--- src/features/cart/CartDetails.jsx
+++ src/features/cart/CartDetails.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import CartDetailsHeader from "../../components/cart/CartDetailsHeader";
 import CartDetailsTableLabel from "../../components/cart/CartDetailsTableLabel";
 import CartDetailsSummary from "../../components/cart/CartDetailsSummary";
 import CartItem from "./CartItem";
 import { useDerivedCart } from "../../hooks/useDerivedCart";
+import { formatCurrency } from "../../utils/numberFormatter";
 
 function CartDetails() {
   const { derivedCart, totalCartCount } = useDerivedCart();
+  const { guaranteedDelivery } = useSelector((store) => store.checkoutReducer);
 
   const cartSubtotal = derivedCart?.reduce((acc, item) => {
     return acc + item.quantity * item.products.price;
@@ -13,8 +16,12 @@ function CartDetails() {
 
   const TAX_RATE = 0.075;
   const VAT = cartSubtotal * TAX_RATE;
-  const shippingFee = "free";
-  const totalAmount = cartSubtotal + VAT;
+  const shippingFee = guaranteedDelivery.date
+    ? guaranteedDelivery.cost
+    : "free";
+  const totalAmount = guaranteedDelivery.date
+    ? cartSubtotal + VAT + shippingFee
+    : cartSubtotal + VAT;
 
   return (
     <section className="flex w-full flex-col items-start justify-center gap-8 pb-5">
@@ -38,7 +45,11 @@ function CartDetails() {
         <CartDetailsSummary
           cartSubtotal={cartSubtotal}
           VAT={VAT}
-          shippingFee={shippingFee}
+          shippingFee={
+            guaranteedDelivery.date
+              ? formatCurrency(shippingFee, 0)
+              : shippingFee
+          }
           totalAmount={totalAmount}
           totalCartCount={totalCartCount}
           variant="page"
