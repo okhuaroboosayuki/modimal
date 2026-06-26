d89b78b9e9b90da799cc76d76569a909406cc07e
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-06-26 01:48:50 +0100

    fix: added SEO component to CheckoutError for improved metadata handling

--- src/pages/CheckoutError.jsx
+++ src/pages/CheckoutError.jsx
@@ -1,40 +1,49 @@
 import { RiErrorWarningFill } from "react-icons/ri";
 import Header from "../components/header/Header";
+import SEO from "../components/SEO";
 
 function CheckoutError() {
   return (
-    <section className="w-full">
-      <Header />
+    <>
+      <SEO
+        title={"Checkout Error - Modimal"}
+        description="Unfortunately, your order could not be completed. Please try again or contact our support team for assistance."
+        url={"/checkout/error"}
+      />
 
-      <section className="flex flex-col items-center justify-center gap-8 px-5 py-10 text-center text-pretty capitalize sm:px-20 sm:py-40 md:px-40 lg:px-52 lg:py-20">
-        <div className="flex flex-col items-center gap-6">
-          <span className="big-icon">
-            <RiErrorWarningFill fill="#c30000" />
-          </span>
+      <section className="w-full">
+        <Header />
 
-          <h2 className="text-error text-3xl font-semibold sm:text-[40px]">
-            sorry, payment failed
-          </h2>
-        </div>
+        <section className="flex flex-col items-center justify-center gap-8 px-5 py-10 text-center text-pretty capitalize sm:px-20 sm:py-40 md:px-40 lg:px-52 lg:py-20">
+          <div className="flex flex-col items-center gap-6">
+            <span className="big-icon">
+              <RiErrorWarningFill fill="#c30000" />
+            </span>
 
-        <div className="flex flex-col items-center gap-4">
-          <p className="sm:ext-[20px] text-lg">
-            unfortunately, your order can not be completed. please try again.
-          </p>
+            <h2 className="text-error text-3xl font-semibold sm:text-[40px]">
+              sorry, payment failed
+            </h2>
+          </div>
 
-          <p className="text-gray40">
-            Contact{" "}
-            <a
-              href="mailto:hello@modimal.com"
-              className="font-medium lowercase!"
-            >
-              hello@modimal.com
-            </a>{" "}
-            for further assistance
-          </p>
-        </div>
+          <div className="flex flex-col items-center gap-4">
+            <p className="sm:ext-[20px] text-lg">
+              unfortunately, your order can not be completed. please try again.
+            </p>
+
+            <p className="text-gray40">
+              Contact{" "}
+              <a
+                href="mailto:hello@modimal.com"
+                className="font-medium lowercase!"
+              >
+                hello@modimal.com
+              </a>{" "}
+              for further assistance
+            </p>
+          </div>
+        </section>
       </section>
-    </section>
+    </>
   );
 }
 
