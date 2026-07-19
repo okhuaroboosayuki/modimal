e829ee1afdc349ed0023bbbb5e86876de7013173
okhuaroboosayuki <osayukiraymond@gmail.com>
2026-07-19 20:46:38 +0100

    feat: added imagetools plugin to Vite configuration for image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    imagetools({
+      defaultDirectives: () =>
+        new URLSearchParams({
+          format: "webp",
+          quality: "85",
+        }),
+    }),
+  ],
 });
